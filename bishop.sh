022eff0c3b73b2107cc8a722e03a62d26b069fbd
stuartervine <stuart.ervine@shift-f6.com>
2016-09-23 08:32:57 +0100

    Adding double tab to replace the bishop command with the actual command.

--- bishop.sh
+++ bishop.sh
@@ -6,11 +6,11 @@ yellow='\033[0;33m'
 no_colour='\033[0m'
 
 function yellow() {
-    echo -e "${yellow}"
+    echo -ne "${yellow}"
 }
 
 function green() {
-    echo -e "${green}"
+    echo -ne "${green}"
 }
 
 function blue() {
@@ -42,27 +42,43 @@ function resolveCommand() {
     echo $command
 }
 
-function _complete() {
-    local cur prev opts
-    COMPREPLY=()
-    currentWord="${COMP_WORDS[COMP_CWORD]}"
-    previousWord="${COMP_WORDS[COMP_CWORD-1]}"
-    selector=".[]"
+function _jsonSelector() {
+    local selector=".[]"
     completedWords=("${COMP_WORDS[@]:1}")
     unset completedWords[${#completedWords[@]}-1]
     for word in ${completedWords[@]}; do selector="$selector.$word"; done
-    commandJson=$(cat $BISHOP_COMMANDS_FILE | jq $selector)
+    echo $selector
+}
+
+function _matchingCommandJson() {
+    local selector=$1
+    echo $(cat $BISHOP_COMMANDS_FILE | jq $selector)
+}
+
+function _complete() {
+    COMPREPLY=()
+    currentWord="${COMP_WORDS[COMP_CWORD]}"
+    selector=$(_jsonSelector)
+    commandJson=$(_matchingCommandJson $selector)
     jsonObjectType=$(echo $commandJson | jq "type")
     if [ $jsonObjectType != "\"string\"" ]; then
+        CURRENT_TAB_COUNT=0
         commands=$(echo $commandJson | jq "keys | .[]" | tr -d "\"" | tr "\n" " ")
         COMPREPLY=( $(compgen -W "${commands}" -- ${currentWord}) )
     else
+        currentCommand=$(resolveCommand $selector)
         tput sc
         yellow
-        echo -ne "   <- $(resolveCommand $selector)"
+        echo -ne "   <- $currentCommand"
         clear
         tput rc
-        COMREPLY=""
+        if [ $CURRENT_TAB_COUNT -eq 2 ]; then
+            ps1=$(PS1="$PS1" "$BASH" --norc -i </dev/null 2>&1 | sed -n '${s/^\(.*\)exit$/\1/p;}')
+            echo "\n"; read -e -p "$ps1$currentCommand" opt; eval "$cmd$opt"
+            return 0
+        else
+            ((CURRENT_TAB_COUNT+=1))
+        fi
     fi
 }
 
